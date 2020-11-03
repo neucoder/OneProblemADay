8e5275606b15a535c9371c9661b153712fc1af42
neucoder <neucoder@163.com>
2020-11-03 16:25:25 +0800

    66

---
+++ leetcode/MaximumSubarray53.java
@@ -0,0 +1,37 @@
+class Solution53 {
+    public int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i], dp[i-1]+nums[i]);
+        }
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < dp.length; i++) {
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
+
+
+public class MaximumSubarray53 {
+
+
+    public static void main(String[] args) {
+
+        Solution53 solution53 = new Solution53();
+        int i = solution53.maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4});
+        System.out.println(i);
+        i = solution53.maxSubArray(new int[]{1});
+        System.out.println(i);
+        i = solution53.maxSubArray(new int[]{0});
+        System.out.println(i);
+        i = solution53.maxSubArray(new int[]{-1});
+        System.out.println(i);
+        i = solution53.maxSubArray(new int[]{-2147483647});
+        System.out.println(i);
+        i = solution53.maxSubArray(new int[]{1});
+        System.out.println(i);
+    }
+
+}
---
+++ leetcode/PlusOne66.java
@@ -0,0 +1,33 @@
+class Solution66 {
+    public int[] plusOne(int[] digits) {
+        digits[digits.length-1] += 1;
+        for (int i = digits.length-1; i >= 1 ; i--) {
+            if(digits[i]>9)
+            {
+                digits[i]%=10;
+                digits[i-1]+=1;
+            }
+        }
+        int[] res = new int[digits.length+1];
+        if(digits[0] > 9)
+        {
+
+            for (int i = 1; i < digits.length; i++) {
+                res[i+1] = digits[i];
+            }
+            res[1] = digits[0]%10;
+            res[0] = 1;
+            return res;
+        }
+
+        return digits;
+    }
+}
+
+public class PlusOne66 {
+    public static void main(String[] args) {
+        Solution66 s  = new Solution66();
+        int[] ints = s.plusOne(new int[]{9,9,9});
+    }
+
+}
