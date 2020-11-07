3a805cf66edb2a96be285e40f378220e82bbe081
neucoder <neucoder@163.com>
2020-11-07 14:32:35 +0800

    849

---
+++ leetcode/MaximizeDistancetoClosestPerson849.java
@@ -0,0 +1,50 @@
+import java.util.Arrays;
+
+class Solution849 {
+    public int maxDistToClosest(int[] seats) {
+        int[] left = new int[seats.length];
+        int[] right = new int[seats.length];
+
+        Arrays.fill(left, seats.length);
+        Arrays.fill(right, seats.length);
+
+        for (int i = 0; i < seats.length; i++) {
+            if(seats[i]==1)
+            {
+                left[i] = 0;
+            }else if(i>0)
+            {
+                left[i] = left[i-1] + 1;
+            }
+
+        }
+
+        for (int i = seats.length-1; i >=0 ; i--) {
+            if(seats[i]==1)
+            {
+                right[i] = 0;
+            }else if(i<seats.length-1)
+            {
+                right[i] = right[i+1]+1;
+            }
+
+        }
+
+        int m = 0;
+        for (int i = 0; i < left.length; i++) {
+            if(seats[i]==0)
+                m = Math.max(m, Math.min(left[i], right[i]));
+        }
+
+        return m;
+    }
+}
+
+public class MaximizeDistancetoClosestPerson849 {
+    public static void main(String[] args) {
+
+        Solution849 s = new Solution849();
+        int i = s.maxDistToClosest(new int[]{1,0,0,0});
+        System.out.println(i);
+    }
+}
---
+++ leetcode/MinimumCosttoMoveChiptoTheSamePosition1217.java
@@ -0,0 +1,27 @@
+
+class Solution1217 {
+    public int minCostToMoveChips(int[] position) {
+        int even = 0;
+        for(int p:position)
+        {
+            if(p%2==0)
+            {
+                even += 1;
+            }
+        }
+        return Math.min(even, position.length - even);
+    }
+}
+
+
+public class MinimumCosttoMoveChiptoTheSamePosition1217 {
+    public static void main(String[] args) {
+        Solution1217 s = new Solution1217();
+        int i = s.minCostToMoveChips(new int[]{1, 2, 3});
+        System.out.println(i);
+        i = s.minCostToMoveChips(new int[]{2,2,2,3,3});
+        System.out.println(i);
+        i = s.minCostToMoveChips(new int[]{1,1000000000});
+        System.out.println(i);
+    }
+}
