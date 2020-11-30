1cff384921bff054a8a800691dbd4b449d381a46
ys <neucoder@163.com>
2020-11-30 22:08:43 +0800

    236

--- leetcode/com/leetcode/problems/Problem236.java
+++ leetcode/com/leetcode/problems/Problem236.java
@@ -2,9 +2,33 @@ package com.leetcode.problems;
 
 class Solution236 {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //baseline 基础情况
+        if(root==null)
+        {
+            return null;
+        }
 
-        return null;
+        if(root==p||root==q)
+        {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left!=null&&right!=null)
+        {
+            return root;
+        }
+        if(left==null&&right==null)
+        {
+            return null;
+        }
+
+        return left==null?right:left;
     }
+
+
 }
 
 
